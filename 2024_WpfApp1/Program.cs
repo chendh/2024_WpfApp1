54f161d1e54ab62508d73b97243a0f023805a01c
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-09-10 16:15:22 +0800

    更新訊息顯示並新增乘法表功能
    原本的程式碼顯示訊息 "視窗程式設計很容易!"，現在改為顯示 "請輸入一個數字："。
    新增了從使用者輸入一個數字的功能，並將其轉換為整數。
    新增了一個巢狀迴圈，根據使用者輸入的數字，輸出對應的乘法表。

--- 2024_WpfApp1/Program.cs
+++ 2024_WpfApp1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("視窗程式設計很容易!");
+            Console.WriteLine("請輸入一個數字：");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i <= n; i++)
+            {
+                for (int j=1; j <= n; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
