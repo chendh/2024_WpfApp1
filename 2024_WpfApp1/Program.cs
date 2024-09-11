7b1162b0d3f22ccf4d5f6f579079fd387c4503e3
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-09-11 10:43:38 +0800

    新增使用者輸入與生成 n 乘 n 乘法表功能
    移除原本的訊息提示，改為提示使用者輸入一個數值 n。新增讀取使用者輸入的程式碼，並使用雙重迴圈生成並顯示 n 乘 n 的乘法表。每行結束後換行，程式結束前再新增一個換行。

--- 2024_WpfApp1/Program.cs
+++ 2024_WpfApp1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("視窗程式設計很容易!");
+            Console.Write("請輸入一個數值n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write($"{i} * {j} = {i * j}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();   
         }
     }
 }
